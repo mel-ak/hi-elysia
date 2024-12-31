781d49c113993a97964f9a07fdc65d1cc27f91a8
mel-ak <melakesisay@gmail.com>
2024-12-31 10:16:11 +0300

    Route Separated

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-import { Elysia } from "elysia";
+import swagger from "@elysiajs/swagger";
+import { Elysia, t } from "elysia";
+import { note } from "./note";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+// Apply the swagger plugin
+.use(swagger()) .get("/", () => "Hello Elysia")
+.use(note) 
+.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
